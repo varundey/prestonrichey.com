386209d1d42509f8fd76e0d549d54077223d56dd
Varun Dey <varundey20@gmail.com>
2019-12-30 22:30:43 +0530

    Add deploy script

--- gh-pages_deploy.js
+++ gh-pages_deploy.js
@@ -3,6 +3,6 @@ const ghPages = require('gh-pages');
 ghPages.publish(
   'public', {
     branch: 'master',
-    repo: 'https://github.com/varundey/varudey.github.io.git',
+    repo: 'https://github.com/varundey/varundey.github.io.git',
   }, _ => console.log('Deploy Complete!')
 );
